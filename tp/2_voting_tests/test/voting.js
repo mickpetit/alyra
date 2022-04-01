90b3ec39dccc6096516e75ccecec15b4f10d38e6
Michael <mpetit@jnesis.com>
2022-04-01 19:55:04 +0200

    Add basics for voting tests

--- tp/2_voting_tests/test/voting.js
+++ tp/2_voting_tests/test/voting.js
@@ -5,7 +5,7 @@ const Voting = artifacts.require('Voting');
 contract('Voting Contract Test Suite', async accounts => {
 
     let instance;
-    const admin = accounts[0];
+    const owner = accounts[0];
     const voter1 = accounts[1];
     const voter2 = accounts[2];
     const voter3 = accounts[3];
@@ -13,16 +13,16 @@ contract('Voting Contract Test Suite', async accounts => {
     const WorkflowStates = Object.freeze({
         'RegisteringVoters':1,
         'ProposalsRegistrationStarted':2,
-        // 'ProposalsRegistrationEnded':3,
-        // 'VotingSessionStarted': 4,
-        // 'VotingSessionEnded': 5,
-        // 'VotesTallied': 6
+        'ProposalsRegistrationEnded':3,
+        'VotingSessionStarted': 4,
+        'VotingSessionEnded': 5,
+        'VotesTallied': 6
     });
 
     const WorkflowInstances = {};
 
     function buildNewInstance() {
-        return Voting.new({ from: admin });
+        return Voting.new({ from: owner });
     }
 
     async function attachVoters(instance) {
@@ -31,11 +31,21 @@ contract('Voting Contract Test Suite', async accounts => {
         await instance.addVoter.call(voter3);
     }
 
-    describe('Initial State "RegisteringVoters"', () => {
-
-        before(async () => {
-            WorkflowInstances[WorkflowStates.RegisteringVoters] = await buildNewInstance();
+    describe('Test basics', () => {
+        it('should be valid for to test mechanism', function () {
+            expect(true).to.be.true;
         });
+        /**
+         * @todo
+         */
+        it('should create a new contract instance', async function () {
+            const instance = await buildNewInstance();
+            // console.debug('instance:', instance)
+        });
+    });
+
+    describe('Test initial state "RegisteringVoters"', () => {
+
 
         describe('Add voters into whitelist', () => {
             it('should be restrict to owner only', () => {
@@ -61,53 +71,58 @@ contract('Voting Contract Test Suite', async accounts => {
         });
     });
 
-    describe('State "ProposalsRegistrationStarted"', () => {
 
-        before(async () => {
-            const instance = await buildNewInstance();
-            attachVoters(instance);
-            await instance.startProposalsRegistering.call();
-            WorkflowInstances[WorkflowStates.ProposalsRegistrationStarted] = instance;
-            console.debug('status:',
-                new BN(await WorkflowInstances[WorkflowStates.RegisteringVoters].workflowStatus.call()).toString()
-            );
-        });
 
-        it('should be completed...', function () {
 
-        });
 
-    });
 
-    describe('States workflow', async () => {
-
-        const WORKFLOW_ALLOWED_MAPPING = [
-            {
-                title: 'add voters',
-                allowed: [WorkflowStates.RegisteringVoters],
-                callback: async (o) => {
-                    if ( o !== undefined ) {
-                        console.debug('workflowStatus: ', new BN(await WorkflowInstances[WorkflowStates.RegisteringVoters].workflowStatus.call()).toString());
-                    }
-
-                    return (o ?? WorkflowInstances[WorkflowStates.RegisteringVoters]).addVoter(voter1)
-                }
-            }
-        ];
-
-        WORKFLOW_ALLOWED_MAPPING.map(action => {
-            // perform allowed match
-            Object.entries(WorkflowStates).filter(([key, value]) => action.allowed.includes(value)).map( state => {
-                it(`should do '${action.title}' on '${state[0]}' state`, async function () {
-                    expect(await action.callback()).to.be.not.empty;
-                });
-            });
-            // perform reject match
-            Object.entries(WorkflowStates).filter(([key, value]) => !action.allowed.includes(value)).map( state => {
-                it(`should not do '${action.title}' on '${state[0]}' state`, async function () {
-                    await expectRevert(action.callback(WorkflowInstances[state[1]]), 'test');
-                });
-            });
-        });
-    });
+    // describe('State "ProposalsRegistrationStarted"', () => {
+    //
+    //     before(async () => {
+    //         const instance = await buildNewInstance();
+    //         attachVoters(instance);
+    //         await instance.startProposalsRegistering.call();
+    //         WorkflowInstances[WorkflowStates.ProposalsRegistrationStarted] = instance;
+    //         console.debug('status:',
+    //             new BN(await WorkflowInstances[WorkflowStates.RegisteringVoters].workflowStatus.call()).toString()
+    //         );
+    //     });
+    //
+    //     it('should be completed...', function () {
+    //
+    //     });
+    //
+    // });
+    //
+    // describe('States workflow', async () => {
+    //
+    //     const WORKFLOW_ALLOWED_MAPPING = [
+    //         {
+    //             title: 'add voters',
+    //             allowed: [WorkflowStates.RegisteringVoters],
+    //             callback: async (o) => {
+    //                 if ( o !== undefined ) {
+    //                     console.debug('workflowStatus: ', new BN(await WorkflowInstances[WorkflowStates.RegisteringVoters].workflowStatus.call()).toString());
+    //                 }
+    //
+    //                 return (o ?? WorkflowInstances[WorkflowStates.RegisteringVoters]).addVoter(voter1)
+    //             }
+    //         }
+    //     ];
+    //
+    //     WORKFLOW_ALLOWED_MAPPING.map(action => {
+    //         // perform allowed match
+    //         Object.entries(WorkflowStates).filter(([key, value]) => action.allowed.includes(value)).map( state => {
+    //             it(`should do '${action.title}' on '${state[0]}' state`, async function () {
+    //                 expect(await action.callback()).to.be.not.empty;
+    //             });
+    //         });
+    //         // perform reject match
+    //         Object.entries(WorkflowStates).filter(([key, value]) => !action.allowed.includes(value)).map( state => {
+    //             it(`should not do '${action.title}' on '${state[0]}' state`, async function () {
+    //                 await expectRevert(action.callback(WorkflowInstances[state[1]]), 'test');
+    //             });
+    //         });
+    //     });
+    // });
 });
